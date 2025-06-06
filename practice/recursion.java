00447eba4c289bda09c9f414028e80aa14c8c52c
KGCAS STUDENTS <casstudent@kgisledu.com>
2025-06-06 09:48:32 +0530

    first version

---
+++ practice/recursion.java
@@ -0,0 +1,19 @@
+package practice;
+
+public class recursion {
+	
+	public static int name(int num)
+	{
+		if(num==0)
+		{
+			return 0;
+		}
+		return num+name(num-1);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(name(10));
+	}
+
+}
